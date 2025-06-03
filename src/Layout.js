024f40493ff3521d4cd9b744ba8b49471c52d35c
de180334 <khoa2k4hg@gmail.com>
2025-06-03 18:47:20 +0700

    fix conflict

--- src/Layout.js
+++ src/Layout.js
@@ -46,42 +46,17 @@ const Layout = () => {
                     <Route path="/signin" element={<SignIn />}/>
                     <Route path="/otp-verify" element={<EnterOTPRegister />} />
                     <Route path="auth/callback" element={<AuthCallback />} />
-                </Routes>
-            </BrowserRouter>
-        </Suspense>
-    );
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Test />}></Route>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/otp-verify" element={<EnterOTPRegister />} />
-          <Route path="auth/callback" element={<AuthCallback />} />
 
-          <Route path="/" element={<Test />}></Route>
+                    <Route path="/" element={<Test />}></Route>
           <Route path="/tutor" element={<TutorListPage />} />
           <Route path="/book/:tutorId" element={<BookingPage />} />
           <Route path="/payment/result" element={<PaymentResult />} />
           <Route path="/user/paymentinfo" element={<PaymentPage />} />
           {/* <Route path="/payment/:bookingId" element={<PaymentResult />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
-  );
+                </Routes>
+            </BrowserRouter>
+        </Suspense>
+    );
 };
 
 export default Layout;
